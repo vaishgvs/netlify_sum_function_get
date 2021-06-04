fa8c81b2b7dd17040cc40805b3fa1e5c6c74057b
Vaishnavi G V S <vaishgvs@gmail.com>
2021-06-04 16:19:08 +0530

    num name cchange

--- netlify/functions/sum.js
+++ netlify/functions/sum.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
-    const num1 = event.queryStringParameters.num1;
-    const num2 = event.queryStringParameters.num2;
+    const num1 = event.queryStringParameters.First_Number;
+    const num2 = event.queryStringParameters.Second_Number;
     
     const sum = parseInt(num1) + parseInt(num2);
     var sum2 = JSON.stringify(sum);
