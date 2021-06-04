5974155b95331cebd6ed3aa359afe090f46dbe48
Vaishnavi G V S <vaishgvs@gmail.com>
2021-06-04 16:12:44 +0530

    returning JSON object

--- netlify/functions/sum.js
+++ netlify/functions/sum.js
@@ -3,9 +3,9 @@ exports.handler = async (event, context) => {
     const num2 = event.queryStringParameters.num2;
     
     const sum = parseInt(num1) + parseInt(num2);
-    const sum2 = toString(sum);
+    const sum2 = JSON.stringify(sum);
     return {
       statusCode: 200,
-      body: `Sum is: ${sum2}`,
+      body: `Sum is: ` + sum2 ,
     };
   };
